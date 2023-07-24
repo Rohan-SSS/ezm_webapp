c7d840e685c78abe5301ea155f75ee5545839909
Rohan-SSS <86891252+Rohan-SSS@users.noreply.github.com>
2023-07-25 02:55:38 +0530

    x

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,6 +12,15 @@ function NavScrollExample() {
             <Container fluid>
                 <Navbar.Brand href="#" className='navbar__brand'>e z m</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
+                <Form className="d-flex ">
+                        <Form.Control
+                            type="search"
+                            placeholder="search an asset..."
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-light" className='navbar__search__button'>search</Button>
+                    </Form>
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
@@ -23,15 +32,7 @@ function NavScrollExample() {
                         <Nav.Link href="#model_specifications" className='navbar__model-button'>model specification</Nav.Link>
                         <Nav.Link href="#about" className='navbar__about-button'>about</Nav.Link>
                     </Nav>
-                    <Form className="d-flex ">
-                        <Form.Control
-                            type="search"
-                            placeholder="search an asset..."
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-light" className='navbar__search__button'>search</Button>
-                    </Form>
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>
